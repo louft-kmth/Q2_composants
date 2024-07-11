6618d51e02f68823934638a7a78a989792b7fbf0
sitraka rajaonarivelo <sitrak@outlook.com>
2024-07-11 11:46:46 +0200

    first_commit

---
+++ src/app/block/block.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.css'
+})
+export class BlockComponent {
+  texte = 'A B C';
+
+}
